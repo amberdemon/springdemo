c2eaa279a1f11234ffc4d6d5abc75a9e798fc64e
Warren Green <warren.green84@gmail.com>
2017-05-09 20:39:47 +0100

    adding json identity info to fix infinite recurssion

--- src/test/java/com/warrengreen/domain/JsonTests.java
+++ src/test/java/com/warrengreen/domain/JsonTests.java
@@ -4,11 +4,14 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonTests {
 
 	private static final Logger log = LoggerFactory.getLogger(JsonTests.class);
 	@Test
-	public void biDirectionalTest() {
+	public void biDirectionalTest() throws JsonProcessingException {
 		
 		User user = new User();
 		user.setId(1);
@@ -18,6 +21,11 @@ public class JsonTests {
 		address.setOwner(user);
 		log.info("User {}",user);
 		
+		
+		String result = new ObjectMapper().writeValueAsString(user);
+		log.info("User {}",result);
+		result = new ObjectMapper().writeValueAsString(address);
+		log.info("Address {}",result);
 	}
 
 }
